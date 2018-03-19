e4efcb3ad00d506e7e999329b9d7d59c08e36190
Applesaurus <hennervate@tutanota.com>
2018-03-19 10:00:49 +0000

    Update fcgi.cpp

--- fcgi.cpp
+++ fcgi.cpp
@@ -13,16 +13,16 @@ class Queries: public Fastcgipp::Request<char> {
 	//with the fastcgi++ library the response method of any request class
 	//seems to be called automatically when it is needed by the manager
 	bool response() {
-		using Fastcgipp::Encoding;
-
-		//
-		MYSQL_RES* result = getQuery();
-		 out << "Content-type: text/html\r\n"
-		 << "\r\n"
-		 << "<html>\n"
-		 << "  <head>\n"
-		 << "    <title>Hello, World!</title>\n"
-		 << "  </head>\n"
+	        using Fastcgipp::Encoding;
+       	        MYSQL_RES* result = getQuery();
+		
+		// just using 'out' should send this to the webserver to send to the client. thanks again fastcgi++
+		out << "Content-type: text/html\r\n"
+	        << "\r\n"
+	        << "<html>\n"
+	        << "  <head>\n"
+	        << "    <title>Hello, World!</title>\n"
+	         << "  </head>\n"
 		 << "  <body>\n"
 		 << "    <h1>" + *result + "</h1>\n"
 		 << "  </body>\n"
@@ -32,38 +32,38 @@ class Queries: public Fastcgipp::Request<char> {
 	}
 	MYSQL_RES* getQuery() {
 
-	    // variables for connecting to the database; note that the username and password are associated with the database
-	    // To Do: these should be set in a config file; database password should be elsewhere not in plaintext;
-	    char* user = "testuser";
-	    char* password = "";
-            char* mySqlSock = "/tmp/mysql/sock";
-	    char* db = "";
-	    char* query;
+                 // variables for connecting to the database; note that the username and password are associated with the database
+	         // To Do: these should be set in a config file; database password should be elsewhere not in plaintext;
+	         char* user = "testuser";
+	         char* password = "";
+                 char* mySqlSock = "/tmp/mysql/sock";
+	         char* db = "";
+	         char* query;
 
-	    // connect to the database
-	    MYSQL* conn = mysql_init ( NULL );
-	    mysql_real_connect ( conn, "localhost", user, password, db, 0, mySqlSock , 0);
+	         // connect to the database
+	         MYSQL* conn = mysql_init ( NULL );
+	         mysql_real_connect ( conn, "localhost", user, password, db, 0, mySqlSock , 0);
 
-	    // several different kinds of queries we will have to choose between
-	    // based on the data supplied by the request
-	    // To Do: build all the query factories
-	    // To Do: needs dynamic field allocation to deal with tags
-	    query = queryFactory.ask(environment().posts);
+	         // several different kinds of queries we will have to choose between
+	         // based on the data supplied by the request
+	         // To Do: build all the query factories
+		 // To Do: needs dynamic field allocation to deal with tags
+	         query = queryFactory.ask(environment().posts);
 
-	    // actually perform the query, save the result
-	    MYSQL_RES* result = mysql_real_query(conn, query, strlen((const char*) query));
-
-	    // pretty sure we gotta remember to close the connection to the database before we can return
-	    mysql_close(conn);
-	    return result;
+	         // actually perform the query, save the result
+	         MYSQL_RES* result = mysql_real_query(conn, query, strlen((const char*) query));
+ 
+	         // pretty sure we gotta remember to close the connection to the database before we can return
+	         mysql_close(conn);
+	         return result;
 	}
 }
 
 int main( int argc, char *argv[]) {
-    Fastcgipp::Manager<Queries> manager;
-    manager.setupSignals();
-    manager.listen();
-    manager.start();
-    manager.join();
-    return 0;
+         Fastcgipp::Manager<Queries> manager;
+         manager.setupSignals();
+         manager.listen();
+         manager.start();
+         manager.join();
+         return 0;
 }
