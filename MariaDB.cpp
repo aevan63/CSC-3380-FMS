89e50986ef39549c976e8d0ee39e71e819b77285
Applesaurus <hennervate@tutanota.com>
2018-04-14 22:26:30 +0000

    Update MariaDB.cpp

--- MariaDB.cpp
+++ MariaDB.cpp
@@ -19,19 +19,19 @@ void MariaDB::close() {
 
 MYSQL_ROW* MariaDB:: stringRES(MYSQL_RES*& result) {
 	MYSQL_ROW row;
-	MYSQL_ROW* rows = new MYSQL_ROW[mysql_num_rows(MYSQL_RES *result)];
-	unsigned int i = 0;
-	while ((row = mysql_fetch_row(result))) {
+	numRows = mysql_num_rows(MYSQL_RES *result);
+	MYSQL_ROW* rows = new MYSQL_ROW[numRows];
+
+	for (unsigned int i = 0; i<numRows; ++i) {
 		unsigned long *lengths;
 		lengths = mysql_fetch_lengths(result);
-		for(unsigned int j=0; j < mysql_num_fields(result); j++) {
+		for(unsigned int j=0; j < mysql_num_fields(result); ++j) {
 			rows[i][j] = row[j];
             //for now lets also have it print for testing
 			printf("[%.*s] ", (int) lengths[j],
 				row[j] ? row[j] : "NULL");			
 		}
 		printf("\n");
-		i++;
 	}
 	return rows;
 }
