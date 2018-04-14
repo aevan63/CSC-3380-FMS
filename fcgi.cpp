23fb3ed5da4ff24d6afb5d9b7136fd8c900a04f4
Applesaurus <hennervate@tutanota.com>
2018-04-14 19:36:50 +0000

    updated to use MariaDBInitializer class

--- fcgi.cpp
+++ fcgi.cpp
@@ -1,3 +1,4 @@
+// Written by Jackie Bowers
 //queries an sql database with requests from a webserver with fastcgi
 //probably doesnt work yet
 
@@ -15,7 +16,7 @@ class Queries: public Fastcgipp::Request<char> {
 	bool response() {
 	        using Fastcgipp::Encoding;
        	        MYSQL_RES* result = getQuery();
-		
+
 		// just using 'out' should send this to the webserver to send to the client. thanks again fastcgi++
 		out << "Content-type: text/html\r\n"
 	        << "\r\n"
@@ -32,17 +33,22 @@ class Queries: public Fastcgipp::Request<char> {
 	}
 	MYSQL_RES* getQuery() {
 
-                // variables for connecting to the database; note that the username and password are associated with the database
+            // variables for connecting to the database;
+			// this is now handled by MariaDBInitializer
 	        // To Do: these should be set in a config file; database password should be elsewhere not in plaintext;
-	        char* user = "testuser";
+	        /*char* user = "testuser";
 	        char* password = "";
-                char* mySqlSock = "/tmp/mysql/sock";
-	        char* db = "";
+            char* mySqlSock = "/tmp/mysql/sock";
+	        char* db = ""; */
 	        char* query;
 
 	        // connect to the database
-	        MYSQL* conn = mysql_init ( NULL );
-                mysql_real_connect ( conn, "localhost", user, password, db, 0, mySqlSock , 0);
+	        MariaDBInitializer init = MariaDBInitializer();
+	        MYSQL* conn = init.connect();
+	        if (0)
+	        	throw "Connection failed!";
+	        else
+	        	cout <<"Connection successful\n"
 
 	        // several different kinds of queries we will have to choose between
 	        // based on the data supplied by the request
@@ -52,7 +58,7 @@ class Queries: public Fastcgipp::Request<char> {
 
 	        // actually perform the query, save the result
 	        MYSQL_RES* result = mysql_real_query(conn, query, strlen((const char*) query));
- 
+
 	        // pretty sure we gotta remember to close the connection to the database before we can return
 	        mysql_close(conn);
 	        return result;
