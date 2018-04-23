ca2177f4a191613f031f635fc58557615f9834cd
Applesaurus <hennervate@tutanota.com>
2018-04-23 00:14:51 +0000

    Completely reworked BuildHTML

--- BuildHTML.h
+++ BuildHTML.h
@@ -5,37 +5,41 @@
 
 #include <iostream>
 #include <fstream>
+#include <libxml/tree.h>
+#include <libxml/HTMLparser.h>
+#include <libxml/tree.h>
+#include <libxml++/libxml++.h>
 #include <tr1/regex>
 #include <mysql.h>
 #include <fastcgi++/request.hpp>
 #include <fastcgi++/http.hpp>
 #include "LocationFeedback.h"
 #include "ProductFeedback.h"
-std::string* parameters = configParse();
+
 #endif
 
 using namespace std;
 
-inline std::string* configParse(){
-	ifstream config;
-	config.open("FMS_Fcgi.conf");
-	string* ruleList = new string[10]; //or something, prolly not 10 lol
-	//do some more stuff
-	return ruleList;
-}
 
-class BuildHTML {
+class buildHTML {
 	private:
-		tr1::regex website;
 		string uri;
 		string html;
-		string* stringRES;
+		htmlDocPtr HTMLdoc;
+		int insertNum;
 
 	public:
-		BuildHTML(const Fastcgipp::Http::Environment& enviroment());
-		BuildHTML(MYSQL_ROW* stringRes, const Fastcgipp::Http::Environment& environment(), std::string website);
-		string* buildFromSQL();
-		string fixHTML(string* HTMLRes);
-		string getHtml();
+		buildHTML(const Fastcgipp::Http::Environment& enviroment(), std::string website);
+
+		buildHTML(MYSQL_ROW* stringRes,int numRows,int* numFields, const Fastcgipp::Http::Environment& environment(), std::string website, std::string feedbackForm);
+
+		htmlNodePtr findInsert(htmlNodePtr root, string matchName);
+
+		void performInsert(htmlNodePtr sibling, xmlChar** newNodes);
+
+		xmlChar** buildFromSQL(MYSQL_ROW* stringRes, int numRows, int* numFields);
+
+		htmlDocPtr getHtml();
+
 		string geturi();
 };
