cabb5d320a314266bdf921e87db4637eff1d2223
Christian Lashover <lashch@att.net>
2018-04-25 23:16:06 -0500

    Reverted changes to MariaDB

--- MariaDB.cpp
+++ MariaDB.cpp
@@ -10,14 +10,19 @@ MariaDBException::MariaDBException(string message) {
 }
 
 MariaDB::MariaDB() throw (MariaDBException) {
-    conn = mysql_init(NULL);
-    mysql_real_connect(conn);
+    MariaDBInitializer init = MariaDBInitializer();
+    try {
+        conn = init.connect();
+    }
+    catch (ConnectionError& c) {
+        throw MariaDBException(c.getError());
+    }
 }
 
 void MariaDB::query(string query) throw (MariaDBException) {
     int state = mysql_real_query(conn, query.c_str(), query.length());
     /*if (state)
-        throw MariaDBException("Query " + query + " failed");*/
+     throw MariaDBException("Query " + query + " failed");*/
     queryResult = mysql_store_result(conn);
 }
 
@@ -26,29 +31,27 @@ void MariaDB::close() {
 }
 
 MYSQL_ROW* MariaDB::stringRES() {
-	MYSQL_ROW row;
-	numRows = mysql_num_rows(queryResult);
-	MYSQL_ROW rows[numRows];
-
-	for (unsigned int i = 0; i<numRows; ++i) {
-		unsigned long *lengths;
-		lengths = mysql_fetch_lengths(queryResult);
-		for(unsigned int j=0; j < mysql_num_fields(queryResult); ++j) {
-			rows[i][j] = row[j];
+    MYSQL_ROW row;
+    numRows = mysql_num_rows(queryResult);
+    MYSQL_ROW rows[numRows];
+    
+    for (unsigned int i = 0; i<numRows; ++i) {
+        unsigned long *lengths;
+        lengths = mysql_fetch_lengths(queryResult);
+        for(unsigned int j=0; j < mysql_num_fields(queryResult); ++j) {
+            rows[i][j] = row[j];
             //for now lets also have it print for testing
-			printf("[%.*s] ", (int) lengths[j],
-				row[j] ? row[j] : "NULL");			
-		}
-		printf("\n");
-	}
-	return rows;
+            printf("[%.*s] ", (int) lengths[j],
+                   row[j] ? row[j] : "NULL");
+        }
+        printf("\n");
+    }
+    return rows;
 }
 int* MariaDB::getNumFields() {
-	int numFields[numRows];
-	for (unsigned int i = 0; i < numRows; ++i) {
-		numFields[i] = mysql_num_fields(queryResult);
-	}
-	return numFields;
+    int numFields[numRows];
+    for (unsigned int i = 0; i < numRows; ++i) {
+        numFields[i] = mysql_num_fields(queryResult);
+    }
+    return numFields;
 }
-
-
