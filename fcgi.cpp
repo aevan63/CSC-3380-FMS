0a6a95b63f2915ea115e2c97a0c710515038f5be
Applesaurus <hennervate@tutanota.com>
2018-04-26 11:55:26 +0000

    ITS OVEERRRR YEAAAAH ITS OOVEERRRRR

--- fcgi.cpp
+++ fcgi.cpp
@@ -93,7 +93,7 @@ private:
 			MYSQL_ROW* rows = sqlObj.stringRES();
 			int* fields = sqlObj.getNumFields();
 			for (int i = 0; i < sqlObj.getNumRows(); ++i) {
-				for (int j = 0; j < fields[i]; ++j) {
+				for (int j = 0; j < fields[0]; ++j) {
 					out << "<h1>" << rows[i][j]<< "</h1>";
 				}
 			}
