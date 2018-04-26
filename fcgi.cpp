b8e020fed8b25b98ed6eee3d24eca0c4d26b1698
Applesaurus <hennervate@tutanota.com>
2018-04-26 05:58:44 +0000

    Update fcgi.cpp

--- fcgi.cpp
+++ fcgi.cpp
@@ -33,7 +33,7 @@ private:
 			 page = BuildHTML(environment().requestUri,website);
 		}
 		else {
-			 page = BuildHTML(environment().gets, environment().requestUri, sqlObj.stringRES(), sqlObj.numRows, sqlObj.getNumFields(), website, formName);
+			 page = BuildHTML(environment().gets, environment().requestUri, sqlObj.stringRES(), sqlObj.getNumRows(), sqlObj.getNumFields(), website, formName);
 		}
 
 	        // out sends page to server w/ fastcgi++
